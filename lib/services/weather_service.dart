72c021a68d19fc66bd5f14ff931ad2180c7b58e1
Revanth <97815884+ThiruvidhiRevanth@users.noreply.github.com>
2024-07-05 10:32:19 +0530

    Update weather_service.dart

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:go_weather/models/weather.dart';
 
 class WeatherService {
-  static const _apiKey = '5f9cd13f81ac9b3464f5b3134a878ca3';
+  static const _apiKey = 'your_api_key';
   static const _baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
   Future<Weather> fetchWeather(String cityName) async {
     final response = await http.get(Uri.parse('$_baseUrl?q=$cityName&units=metric&appid=$_apiKey'));
